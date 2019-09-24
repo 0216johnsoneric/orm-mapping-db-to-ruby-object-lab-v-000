f0652359a1672cfb1c1989cc59fba4b3833e6975
Eric Johnson <ericjohnson0216@icloud.com>
2019-09-24 03:36:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,7 +51,7 @@ end
 
   def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
-      SELECT * FROM students WHERE grade = 10 LIMIT = ?
+      SELECT * FROM students WHERE grade = 10 GROUP BY = id LIMIT = ? 
     SQL
     DB[:conn].execute(sql, x).collect do |row|
       self.new_from_db(row)
