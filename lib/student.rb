276a4623b76c26a58642dd72e7bb80dfb2d614cc
Eric Johnson <ericjohnson0216@icloud.com>
2019-09-24 03:17:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,22 +51,22 @@ end
 
   def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
-      SELECT * FROM students WHERE grade = 10 
+      SELECT * FROM students WHERE grade = 10
     SQL
     DB[:conn].execute(sql, x).collect do |row|
       self.new_from_db(row)
     end.first
   end
 
-  def self.first_student_in_grade_10
-    sql = <<-SQL
-    SELECT * FROM students WHERE grade = 10
-    LIMIT 1
-    SQL
-    DB[:conn].execute(sql).collect do |row|
-      self.new_from_db(row)
-    end.first
-  end	  end
+  # def self.first_student_in_grade_10
+  #   sql = <<-SQL
+  #   SELECT * FROM students WHERE grade = 10
+  #   LIMIT 1
+  #   SQL
+  #   DB[:conn].execute(sql).collect do |row|
+  #     self.new_from_db(row)
+  #   end.first
+  # end	  end
 
 
   def save
